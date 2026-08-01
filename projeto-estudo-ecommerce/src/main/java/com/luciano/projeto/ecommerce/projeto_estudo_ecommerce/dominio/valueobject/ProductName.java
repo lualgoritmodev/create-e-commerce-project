61b155fdfd7ca6c3575c5c9d31e1ee752b9b02e2
lualgoritmodev <lualgoritmo5@gmail.com>
2026-08-01 20:16:36 -0300

    refatorar ProductName

--- projeto-estudo-ecommerce/src/main/java/com/luciano/projeto/ecommerce/projeto_estudo_ecommerce/dominio/valueobject/ProductName.java
+++ projeto-estudo-ecommerce/src/main/java/com/luciano/projeto/ecommerce/projeto_estudo_ecommerce/dominio/valueobject/ProductName.java
@@ -2,7 +2,7 @@ package com.luciano.projeto.ecommerce.projeto_estudo_ecommerce.dominio.valueobje
 
 import java.util.regex.Pattern;
 
-public final class ProductName {
+public record ProductName(String value) {
 
     private static final int MIN_LENGTH = 3;
     private static final int MAX_LENGTH = 100;
@@ -13,28 +13,37 @@ public final class ProductName {
     private static final Pattern CONTAINS_LETTER_OR_NUMBER =
             Pattern.compile(".*[\\p{L}\\p{N}].*");
 
-    private final String value;
+    public ProductName { value = validateAndNormalize(value); }
 
-    public ProductName(String value) {
-        this.value = validateAndNormalize(value);
-    }
     private static String validateAndNormalize(String value) {
-        if (value == null) {
+
+        validateRequired(value);
+
+        String normalized = normalize(value);
+        validateNotBlank(normalized);
+        validateLength(normalized);
+        validateContent(normalized);
+
+        return normalized;
+    }
+    private static void validateRequired(String value) {
+        if(value == null) {
             throw new IllegalArgumentException(
                     "O nome do produto é obrigatório."
             );
         }
+    }
 
-        String normalized = normalize(value);
+    private static void validateNotBlank(String value) {
+            if (value.isBlank()) {
+                throw new IllegalArgumentException(
+                        "O nome do produto não pode estar vazio."
+                );
+            }
+    }
 
-        if (normalized.isBlank()) {
-            throw new IllegalArgumentException(
-                    "O nome do produto não pode estar vazio."
-            );
-        }
-
-        if (normalized.length() < MIN_LENGTH
-                || normalized.length() > MAX_LENGTH) {
+    private static void validateLength(String value) {
+        if (value.length() < MIN_LENGTH || value.length() > MAX_LENGTH) {
             throw new IllegalArgumentException(
                     "O nome do produto deve ter entre "
                             + MIN_LENGTH
@@ -43,28 +52,21 @@ public final class ProductName {
                             + " caracteres."
             );
         }
+    }
 
-        if (!CONTAINS_LETTER_OR_NUMBER
-                .matcher(normalized)
-                .matches()) {
+    private static void validateContent(String value) {
+        if (!CONTAINS_LETTER_OR_NUMBER.matcher(value).matches()) {
             throw new IllegalArgumentException(
                     "O nome do produto deve conter ao menos uma letra ou número."
             );
         }
-
-        return normalized;
     }
-
     private static String normalize(String value) {
         String trimmedValue = value.trim();
 
         return MULTIPLE_SPACES
                 .matcher(trimmedValue)
                 .replaceAll(" ");
-    }
-
-    public String getValue() {
-        return value;
     }
 
 }
