b502b10dc0f17960d776208073768dcd82db4380
lualgoritmodev <lualgoritmo5@gmail.com>
2026-08-06 22:48:06 -0300

    Finalizar testes ProductName

--- projeto-estudo-ecommerce/src/test/java/com/luciano/projeto/ecommerce/projeto_estudo_ecommerce/domain/ProductNameTest.java
+++ projeto-estudo-ecommerce/src/test/java/com/luciano/projeto/ecommerce/projeto_estudo_ecommerce/domain/ProductNameTest.java
@@ -2,16 +2,42 @@ package com.luciano.projeto.ecommerce.projeto_estudo_ecommerce.domain;
 import com.luciano.projeto.ecommerce.projeto_estudo_ecommerce.domain.valueobject.ProductName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductNameTest {
-
     @Test
     void shouldCreateProductNameWhenValueIsValid() {
         ProductName expect = new ProductName("Notebook Gamer");
+
         assertEquals("Notebook Gamer", expect.value());
         assertNotEquals(" ", expect.value());
+
+    }
+    @Test
+    void shouldNormalizeProductName() {
+        ProductName expect = new ProductName(" Notebook    Gamer ");
+
+        assertEquals("Notebook Gamer", expect.value());
+    }
+    @Test
+    void shouldThrowExceptionWhenValueIsBlank() {
+        IllegalArgumentException exception = assertThrows(
+           IllegalArgumentException.class,
+                () -> new ProductName("  ")
+        );
+
+        assertEquals("O nome do produto não pode estar vazio.", exception.getMessage());
+    }
+    @Test
+    void shouldThrowExceptionWhenValueIsNull() {
+
+       IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new ProductName(null)
+        );
+
+       assertEquals("O nome do produto é obrigatório.", exception.getMessage());
+
     }
 
 }
